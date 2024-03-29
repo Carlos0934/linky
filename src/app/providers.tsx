45681216ffe65a381d079b1d70e2f3ef9f6c3ec7
Carlos Olivo <carlosengel011@gmail.com>
2024-03-28 23:31:56 -0400

    Remove unused QueryClientProvider from Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,10 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 type ProviderProps = {
   children: React.ReactNode;
 };
-const queryClient = new QueryClient();
 
 export default function Providers({ children }: ProviderProps) {
-  return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
