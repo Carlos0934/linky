4e99bc2343d52375621d38e051e4d7f2a9895e89
Carlos Olivo <carlosengel011@gmail.com>
2024-03-28 23:46:23 -0400

    Update sign-in button to include callback URL

--- src/app/components/sign-in-button.tsx
+++ src/app/components/sign-in-button.tsx
@@ -4,7 +4,11 @@ import { signIn } from "next-auth/react";
 export default function SignInButton() {
   return (
     <button
-      onClick={() => signIn("github")}
+      onClick={() =>
+        signIn("github", {
+          callbackUrl: `${window.location.origin}/api/auth/callback`,
+        })
+      }
       className="px-6 py-2.5  bg-blue-700 shadow-blue-800 shadow-md  rounded-full font-semibold text-gray-200 hover:bg-blue-600 transition-colors duration-300 ease-in-out"
     >
       Sign in with GitHub
